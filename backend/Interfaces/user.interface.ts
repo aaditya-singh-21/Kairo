4cb1bb67e5645c0a17e3c0d4fe1036133828edab
Aaditya <aaditya2110s@gmail.com>
2026-05-23 16:24:56 +0530

    Initial setup

---
+++ backend/Interfaces/user.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+
+
+export interface IUser {
+    _id: Types.ObjectId,
+    name: string,
+    password?: string,
+    email: string,
+    googleId?: string,
+    avatar?: string,
+    bio?: string,
+    tech?: string[],
+    socials?: {
+        github?: string,
+        linkedin?: string,
+        portfolio?: string
+    }
+}
